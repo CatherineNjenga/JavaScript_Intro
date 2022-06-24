6fc492debe63c5ae55a7cf2fce4be129d984993c
Freshiah Catherine Njenga <87461018+CatherineNjenga@users.noreply.github.com>
2022-06-24 13:10:03 +0300

    Changing the JavaScript file

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let answer = prompt("Should I tell you about myself? (respond with 'y' or 'n').")
+let answer = window.prompt("Should I tell you about myself? (respond with 'y' or 'n').")
 
 let myName = 'Catherine';
 let myHeight = 173;
 let myCountry = 'Kenya';
 
-if (response == 'y') {
+if (answer == 'y') {
     window.alert(`My name is ${myName}.`);
     window.alert(`I am ${myHeight} cm.`);
     window.alert(`I am from ${myCountry}.`);
-} else if (response == 'n') {
+} else if (answer == 'n') {
     
 }
 
