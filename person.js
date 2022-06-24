f98a48c5269579acbee2da19a6bde9c58c6396f5
Freshiah Catherine Njenga <87461018+CatherineNjenga@users.noreply.github.com>
2022-06-24 12:09:55 +0300

    "Making the alert interactive"

--- person.js
+++ person.js
@@ -2,6 +2,12 @@ let myName = "Catherine";
 let myHeight = 173;
 let myCountry = "Kenya";
 
-window.alert(`My name is ${myName}.`);
-window.alert(`I am ${myHeight} cm.`);
-window.alert(`I am from ${myCountry}.`);
+let response = prompt("Should I tell you about me? (respond with 'y' or 'n').");
+
+if (response == 'y') {
+    window.alert(`My name is ${myName}.`);
+    window.alert(`I am ${myHeight} cm.`);
+    window.alert(`I am from ${myCountry}.`);
+} else if (response == 'n') {
+}
+
