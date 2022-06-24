33e4d13a3ff449bdb3ab456300b1b2aa6a37fa3a
Freshiah Catherine Njenga <87461018+CatherineNjenga@users.noreply.github.com>
2022-06-24 12:49:46 +0300

    Interactive prompt

--- person.js
+++ person.js
@@ -1,9 +1,14 @@
 let response = prompt("Should I tell you about myself? (respond with 'y' or 'n').");
 
+let myName = 'Catherine';
+let myHeight = 173;
+let myCountry = 'Kenya';
+
 if (response == 'y') {
     window.alert(`My name is ${myName}.`);
     window.alert(`I am ${myHeight} cm.`);
     window.alert(`I am from ${myCountry}.`);
 } else if (response == 'n') {
+    
 }
 
