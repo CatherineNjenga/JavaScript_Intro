713e8e0e01542c1d0b3ba99073069621e1c6905c
Freshiah Catherine Njenga <87461018+CatherineNjenga@users.noreply.github.com>
2022-06-24 20:58:51 +0300

    Updating the js file

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-let answer = prompt("Should I tell you about myself? (respond with 'y' or 'n').");
-
 let myName = 'Catherine';
 let myHeight = 173;
 let myCountry = 'Kenya';
 
-if (answer == 'y') {
-    alert(`My name is ${myName}.`);
-    alert(`I am ${myHeight} cm.`);
-    alert(`I am from ${myCountry}.`);
-} else if (answer == 'n') {
-    
-}
+alert(`My name is ${myName}.`);
+alert(`I am ${myHeight} cm.`);
+alert(`I am from ${myCountry}.`);
+
 
 
 
